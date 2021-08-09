734d39c6f1d0982ae3878618083527376bc401c6
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-08-09 09:21:11 +0200

    chore: add testing-library/react

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+    /*    preset: "ts-jest", */
+    testEnvironment: "jsdom",
+    clearMocks: true,
+    /* coverageProvider: "v8", */
+    setupFilesAfterEnv: ["<rootDir>/jest/jest.setup.js"],
     collectCoverageFrom: [],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+        "^.+\\.svg$": "<rootDir>/jest/svgTransform.js",
+        "^.+\\.css$": "<rootDir>/jest/cssTransform.js"
+    },
     globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.jest.json',
+        "ts-jest": {
+            tsconfig: "<rootDir>/jest/tsconfig.jest.json",
             diagnostics: false
         }
     }
