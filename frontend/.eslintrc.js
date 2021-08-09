f6a3d5a2cbb55348d8c022f7a378de86d254cc60
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-08-09 15:49:25 +0200

    chore: improve eslint frontend

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
-    extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+    extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
+    ],
+    env: {
+        browser: true,
+        es2021: true
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: "module"
+    },
+    plugins: ["react", "@typescript-eslint"],
     rules: {
         "@typescript-eslint/interface-name-prefix": "off",
         "@typescript-eslint/no-explicit-any": "off",
@@ -13,5 +29,11 @@ module.exports = {
                 endOfLine: "auto"
             }
         ]
+    },
+    settings: {
+        react: {
+            pragma: "React",
+            version: "detect"
+        }
     }
 };
